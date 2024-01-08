79bccb53307ab184173d95a6d0815d642b7e9af9
Javier Rocha Gómez <javierrg1992@gmail.com>
2024-01-08 19:40:10 +0100

    Reserva, Habitacion, Hotel class implementation

---
+++ models/objects/Hotel.php
@@ -0,0 +1,89 @@
+<?php
+class Hotel{
+    private $id;
+    private $nombre;
+    private $direccion;
+    private $ciudad;
+    private $pais;
+    private $num_habitacioes;
+    private $descripcion;
+    private $foto;
+    
+    function __construct($id,$nombre,$direccion,$ciudad,$pais,$num_habitaciones,$descripcion,$foto){
+        $this->id = $id;
+        $this->nombre =$nombre;
+        $this->direccion =$direccion;
+        $this->ciudad =$ciudad;
+        $this->pais =$pais;
+        $this->num_habitacioes =$num_habitaciones;
+        $this->descripcion = $descripcion;
+        $this->foto =$foto;
+    }
+    
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function getDireccion() {
+        return $this->direccion;
+    }
+
+    public function getCiudad() {
+        return $this->ciudad;
+    }
+
+    public function getPais() {
+        return $this->pais;
+    }
+
+    public function getNum_habitacioes() {
+        return $this->num_habitacioes;
+    }
+
+    public function getDescripcion() {
+        return $this->descripcion;
+    }
+
+    public function getFoto() {
+        return $this->foto;
+    }
+
+    public function setId($id): void {
+        $this->id = $id;
+    }
+
+    public function setNombre($nombre): void {
+        $this->nombre = $nombre;
+    }
+
+    public function setDireccion($direccion): void {
+        $this->direccion = $direccion;
+    }
+
+    public function setCiudad($ciudad): void {
+        $this->ciudad = $ciudad;
+    }
+
+    public function setPais($pais): void {
+        $this->pais = $pais;
+    }
+
+    public function setNum_habitacioes($num_habitacioes): void {
+        $this->num_habitacioes = $num_habitacioes;
+    }
+
+    public function setDescripcion($descripcion): void {
+        $this->descripcion = $descripcion;
+    }
+
+    public function setFoto($foto): void {
+        $this->foto = $foto;
+    }
+
+
+    
+}
